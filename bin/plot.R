704840360fc5be2f55d635206ce5425cf66ed524
Kris Selden <kris.selden@gmail.com>
2018-02-02 14:43:47 -0800

    plot runtime stats

--- bin/plot.R
+++ bin/plot.R
@@ -37,6 +37,59 @@ print(
     labs(title = 'Phase Durations')
 )
 
+if (!is.null(r$stats)) {
+  library(dplyr)
+  library(forcats)
+  library(tidyr)
+
+  plotStats <- function(df) {
+    print(
+      ggplot(df, aes(set, value)) +
+        facet_wrap(~stat+metric, ncol=2, scales = "free", strip.position="left") +
+        geom_boxplot(outlier.shape = NA) +
+        geom_jitter(alpha=0.5, width=0.2, aes(color=set)) +
+        coord_flip() +
+        theme(
+          axis.title = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          strip.text = element_text(size = 8))
+    )
+  }
+
+  filterLevels <- function(df, lvls) {
+    df <- filter(df, stat %in% lvls)
+    df$stat <- factor(df$stat, levels=lvls)
+    df
+  }
+
+  pageStats <- function(df) {
+    lvls <- levels(df$stat)
+    len <- length(lvls)
+    for (i in seq(1,len,by=5)) {
+      plotStats(filterLevels(df, lvls[i:min(i+4, len)] ))
+    }
+  }
+
+  df <- r$stats %>%
+    group_by(set, group, sample) %>%
+    summarise(count = sum(count), ms = sum(ms)) %>%
+    rename(stat = group)
+  df$stat <- fct_reorder(df$stat, df$ms, median, .desc = TRUE)
+
+  df <- gather(df, metric, value, c(count, ms))
+
+  pageStats(df)
+
+  df <- r$stats %>%
+    group_by(set, stat, sample) %>%
+    summarise(count = sum(count), ms = sum(ms))
+  df$stat <- fct_reorder(df$stat, df$ms, median, .desc = TRUE)
+  df <- gather(df, metric, value, c(count, ms))
+
+  pageStats(df)
+}
+
 pairs <- r$set.pairs
 for (i in seq_len(nrow(pairs))) {
   pair <- pairs[i,]
