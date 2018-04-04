4ba4d285e396a46cea00059037a90346ccf61341
Kris Selden <kris.selden@gmail.com>
2018-04-03 19:38:43 -0700

    handle mainProcess in Trace. add loadTrace method. add `yarn build` and `yarn watch`
    Former-commit-id: 47f21d7ec5dfa9ad526c75fcc5f4d0d8d3da0219

--- packages/parse-profile/src/index.ts
+++ packages/parse-profile/src/index.ts
@@ -1,2 +1,11 @@
+import { ITraceEvent, Trace } from './trace';
+
 export { Trace } from './trace';
 export { default as CpuProfile } from './cpuprofile';
+
+export function loadTrace(events: ITraceEvent[]) {
+  let trace = new Trace();
+  trace.addEvents(events);
+  trace.buildModel();
+  return trace;
+}
