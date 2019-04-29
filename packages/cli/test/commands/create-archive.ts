511cb699c059a4f3053d22e08a45a63652037e83
Marc Lynch <lynchmarc2@gmail.com>
2019-04-29 15:26:40 -0700

    cli: compare test from tb-config

--- packages/cli/test/commands/create-archive.ts
+++ packages/cli/test/commands/create-archive.ts
@@ -1,12 +1,19 @@
 import { test } from '@oclif/test';
 import * as chai from 'chai';
 import * as path from 'path';
+import * as fs from 'fs-extra';
 import CreateArchive from '../../src/commands/create-archive';
 import { tmpDir } from '../setup';
 chai.use(require('chai-fs'));
 
+const tbConfig = JSON.parse(
+  fs.readFileSync(`${process.cwd()}/test/tbconfig.json`, 'utf8')
+);
 const url = 'https://www.tracerbench.com';
 const archiveOutput = path.join(`${process.cwd()}/${tmpDir}/trace.har`);
+const tbConfigArchiveOutput = path.join(
+  `${process.cwd()}/${tmpDir}/${tbConfig.archiveOutput}`
+);
 
 describe('create-archive', () => {
   test
@@ -18,10 +25,30 @@ describe('create-archive', () => {
           '--url',
           url,
           '--archiveOutput',
-          archiveOutput
+          archiveOutput,
         ]);
         chai.expect(ctx.stdout).to.contain(`HAR successfully generated`);
         chai.expect(archiveOutput).to.be.a.file();
+      }
+    );
+});
+
+describe('create-archive with tb-config.json flags', () => {
+  test
+    .stdout()
+    .it(
+      `runs create-archive --url ${
+        tbConfig.url
+      } --archiveOutput ${tbConfigArchiveOutput}`,
+      async ctx => {
+        await CreateArchive.run([
+          '--url',
+          tbConfig.url,
+          '--archiveOutput',
+          tbConfigArchiveOutput,
+        ]);
+        chai.expect(ctx.stdout).to.contain(`HAR successfully generated`);
+        chai.expect(tbConfigArchiveOutput).to.be.a.file();
       }
     );
 });
