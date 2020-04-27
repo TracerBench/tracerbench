2d18980d4c5f4f69e7026141e5bffbe7930f29fb
Marc Lynch <lynchmarc2@gmail.com>
2020-04-27 14:16:06 -0700

    refactor(cli): profile cmd use listr

--- packages/cli/src/commands/profile.ts
+++ packages/cli/src/commands/profile.ts
@@ -1,13 +1,13 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import {
   analyze,
-  IAnalyze,
   IConditions,
   ITraceEvent,
   liveTrace,
   loadTrace,
 } from "@tracerbench/core";
+import Protocol from "devtools-protocol";
 import { existsSync, mkdirSync, readJson, writeFileSync } from "fs-extra";
+import * as listr from "listr";
 import { join, resolve } from "path";
 
 import { TBBaseCommand } from "../command-config";
@@ -35,9 +35,19 @@ import {
   setTraceEvents,
 } from "../helpers/utils";
 
+interface ProfileContext {
+  cookies: Protocol.Network.CookieParam[];
+  harJSON: any;
+  traceJSONPath: string;
+  traceEvents: ITraceEvent[];
+  url: string;
+  analyzeResults: { node: string; hierarchyReports: string[] };
+}
+
 export default class Profile extends TBBaseCommand {
   // include backwards compat to trace cmd
   static aliases = ["trace"];
+  public trace: ITraceEvent[] = [];
   public static description = `Parses a CPU profile and aggregates time across heuristics.`;
   public static args = [harpath];
   public static flags = {
@@ -49,7 +59,7 @@ export default class Profile extends TBBaseCommand {
     usertimings,
   };
 
-  public async run() {
+  public async run(): Promise<void> {
     const { flags, args } = this.parse(Profile);
     const {
       cpuThrottleRate,
@@ -57,10 +67,10 @@ export default class Profile extends TBBaseCommand {
       tbResultsFolder,
       network,
       usertimings,
+      url,
     } = flags;
     const { harpath } = args;
-    let { url } = flags;
-    let cookies = [
+    const cookies = [
       {
         name: "",
         value: "",
@@ -73,98 +83,147 @@ export default class Profile extends TBBaseCommand {
       cpu: cpuThrottleRate,
       network,
     };
-    if (cookiespath.length) {
-      cookies = await readJson(resolve(cookiespath));
-    }
-    // if the folder for the tracerbench results file
-    // does not exist then create it
-    try {
-      if (!existsSync(tbResultsFolder)) {
-        mkdirSync(tbResultsFolder);
-      }
-    } catch (error) {
-      this.error(error);
-    }
-    // validate har and clone it to trace.json which will be mutated
-    const harJSON = await readJson(resolve(harpath));
-    const traceJSONPath = join(tbResultsFolder, "trace.json");
-    writeFileSync(traceJSONPath, JSON.stringify(harJSON, null, 2));
 
-    // if no url get url from har
-    if (!url.length) {
-      try {
-        url = harJSON.log.entries[0].request.url;
-      } catch (error) {
-        this.error(
-          `${error}. Could not extract the URL from the HAR. Explicitly pass via "--url" flag and rerun.`
-        );
-      }
-    }
+    const tasks = new listr([
+      {
+        title: "Reading cookies json",
+        task: async (ctx: ProfileContext) => {
+          // read cookies json file from path and set to context
+          ctx.cookies = cookiespath.length
+            ? await readJson(resolve(cookiespath))
+            : cookies;
+        },
+      },
+      {
+        title: "Preparing results folder",
+        task: () => {
+          // if the folder for the tracerbench results file
+          // does not exist then create it
+          try {
+            if (!existsSync(tbResultsFolder)) {
+              mkdirSync(tbResultsFolder);
+            }
+          } catch (error) {
+            this.error(error);
+          }
+        },
+      },
+      {
+        title: "Validating & Cloning HAR file",
+        task: async (ctx: ProfileContext) => {
+          // validate har and clone it to trace.json which will be mutated
+          ctx.harJSON = await readJson(resolve(harpath));
+          ctx.traceJSONPath = join(tbResultsFolder, "trace.json");
+          writeFileSync(
+            ctx.traceJSONPath,
+            JSON.stringify(ctx.harJSON, null, 2)
+          );
+          // if no url get url from har otherwise set with cmd url
+          ctx.url = url.length ? url : getURLFromHAR(ctx);
 
-    // run the liveTrace
-    const { traceEvents } = await liveTrace(
-      url,
-      tbResultsFolder,
-      cookies,
-      conditions
-    );
+          function getURLFromHAR(ctx: ProfileContext): string {
+            try {
+              const url = ctx.harJSON.log.entries[0].request.url;
+              return url;
+            } catch (error) {
+              throw new Error(
+                `${error}. Could not extract the URL from the HAR. Explicitly pass via "--url" flag and rerun.`
+              );
+            }
+          }
+        },
+      },
+      {
+        title: "Recording the live trace",
+        task: async (ctx: ProfileContext) => {
+          const { cookies, url } = ctx;
+          try {
+            // run the liveTrace
+            const { traceEvents } = await liveTrace(
+              url,
+              tbResultsFolder,
+              cookies,
+              conditions
+            );
+            ctx.traceEvents = traceEvents;
+          } catch (error) {
+            this.error(`${error}`);
+          }
+        },
+      },
+      {
+        title: "Analyzing the live trace",
+        task: async (ctx: ProfileContext) => {
+          const { traceEvents, harJSON } = ctx;
 
-    const analyzeOptions: IAnalyze = {
-      traceEvents,
-      traceHARJSON: harJSON,
-      methods,
-    };
+          try {
+            // analyze the liveTrace
+            ctx.analyzeResults = await analyze({
+              traceEvents,
+              traceHARJSON: harJSON,
+              methods,
+            });
+          } catch (error) {
+            this.error(`${error}`);
+          }
+        },
+      },
+      {
+        title: "Setting trace events",
+        task: async (ctx: ProfileContext) => {
+          const { traceEvents } = ctx;
+          // mutates this.trace
+          try {
+            this.trace = setTraceEvents(traceEvents);
+          } catch (error) {
+            this.error(`${error}`);
+          }
+        },
+      },
+    ]);
 
-    // analyze the liveTrace
-    await analyze(analyzeOptions);
+    await tasks
+      .run()
+      .catch((error) => {
+        this.error(`${error}`);
+      })
+      .then(async (ctx) => {
+        // log logAnalyzeReports
+        this.log(`\n`);
+        this.logAnalyzeReports(ctx.analyzeResults);
+        // log js-eval-time
+        this.log(`\n`);
+        this.logJSEvalTime();
+        // log css-parse
+        this.log(`\n`);
+        this.logCSSEvalTime();
+        // log list-functions
+        this.log(`\n`);
+        this.listFrames();
+        // log user timings
+        if (usertimings) {
+          this.log(`\n`);
+          await this.markerTimings(ctx.traceJSONPath, ctx.url);
+        }
+      });
+  }
 
-    // js-eval-time
-    let trace: any;
-    let totalJSDuration = 0;
-    let totalCSSDuration = 0;
+  private logAnalyzeReports(analyzeResults: {
+    node: string;
+    hierarchyReports: string[];
+  }): void {
+    this.log(`${analyzeResults.node}`);
+    analyzeResults.hierarchyReports.forEach((report) => {
+      this.log(`${report}`);
+    });
+  }
 
+  private listFrames(): void {
     const methodsSet = new Set();
+    let clonedTrace: any = null;
 
     try {
-      trace = setTraceEvents(traceEvents);
-    } catch (error) {
-      this.error(`${error}`);
-    }
-
-    trace
-      .filter((event: ITraceEvent) => event.name === "EvaluateScript")
-      .filter((event: any) => event.args.data.url)
-      .forEach((event: any) => {
-        const url = event.args.data.url;
-        const durationInMs = (event.dur as number) / 1000;
-        totalJSDuration += durationInMs;
-        this.log(`JS: ${url}: ${durationInMs.toFixed(2)}`);
-      });
-
-    // log js-eval-time
-    this.log(
-      `JS: Evaluation Total Duration: ${totalJSDuration.toFixed(2)}ms \n\n`
-    );
-
-    // css-parse
-    trace
-      .filter((event: ITraceEvent) => event.name === "ParseAuthorStyleSheet")
-      .filter((event: any) => event.args.data.styleSheetUrl)
-      .forEach((event: any) => {
-        const url = event.args.data.styleSheetUrl;
-        const durationInMs = event.dur / 1000;
-        totalCSSDuration += durationInMs;
-        this.log(`CSS: ${url}: ${durationInMs.toFixed(2)}`);
-      });
-
-    // log css-parse-time
-    this.log(
-      `CSS: Evaluation Total Duration: ${totalCSSDuration.toFixed(2)}ms \n\n`
-    );
-
-    // list-functions
-    try {
-      const profile = loadTrace(trace).cpuProfile(-1, -1);
+      const profile = loadTrace(this.trace).cpuProfile(-1, -1);
       profile.nodeMap.forEach((node: any) => {
         const { functionName, url, lineNumber, columnNumber } = node.callFrame;
 
@@ -179,8 +238,8 @@ export default class Profile extends TBBaseCommand {
     }
 
     try {
-      trace = setTraceEvents(traceEvents);
-      const traceLoad = trace.filter(isCommitLoad);
+      clonedTrace = this.trace;
+      const traceLoad = clonedTrace.filter(isCommitLoad);
       traceLoad.forEach(
         ({
           args: {
@@ -195,10 +254,42 @@ export default class Profile extends TBBaseCommand {
     } catch (error) {
       this.error(`${error}`);
     }
+  }
 
-    if (usertimings) {
-      await this.markerTimings(traceJSONPath, url);
-    }
+  private logCSSEvalTime(): void {
+    let totalCSSDuration = 0;
+
+    this.trace
+      .filter((event: ITraceEvent) => event.name === "ParseAuthorStyleSheet")
+      .filter((event: any) => event.args.data.styleSheetUrl)
+      .forEach((event: any) => {
+        const url = event.args.data.styleSheetUrl;
+        const durationInMs = event.dur / 1000;
+        totalCSSDuration += durationInMs;
+        this.log(`CSS: ${url}: ${durationInMs.toFixed(2)}`);
+      });
+
+    // log css-parse-time
+    this.log(
+      `CSS: Evaluation Total Duration: ${totalCSSDuration.toFixed(2)}ms`
+    );
+  }
+
+  private logJSEvalTime(): void {
+    let totalJSDuration = 0;
+
+    this.trace
+      .filter((event: ITraceEvent) => event.name === "EvaluateScript")
+      .filter((event: any) => event.args.data.url)
+      .forEach((event: any) => {
+        const url = event.args.data.url;
+        const durationInMs = (event.dur as number) / 1000;
+        totalJSDuration += durationInMs;
+        this.log(`JS: ${url}: ${durationInMs.toFixed(2)}`);
+      });
+
+    // log js-eval-time
+    this.log(`JS: Evaluation Total Duration: ${totalJSDuration.toFixed(2)}ms`);
   }
 
   // tracerbench marker-timings cmd moved here
@@ -212,7 +303,7 @@ export default class Profile extends TBBaseCommand {
     let frame: any = null;
     let startTime = -1;
     let rawTraceData: any = null;
-    let trace: any = null;
+    let customTrace: any = null;
 
     if (!url) {
       this.error(
@@ -227,13 +318,13 @@ export default class Profile extends TBBaseCommand {
     }
 
     try {
-      trace = setTraceEvents(rawTraceData);
+      customTrace = setTraceEvents(rawTraceData);
     } catch (e) {
       this.error(e);
     }
 
     if (traceFrame.startsWith("http")) {
-      frame = findFrame(trace, traceFrame);
+      frame = findFrame(customTrace, traceFrame);
     } else {
       frame = traceFrame;
     }
@@ -243,7 +334,7 @@ export default class Profile extends TBBaseCommand {
       );
     }
 
-    trace
+    customTrace
       .filter((event: IEvent) => isMark(event) || isCommitLoad(event))
       .sort(byTime)
       .forEach((event: any) => {
