bb571ce7ca1a95e5bd3fbe2520c20681b6a1719f
Marc Lynch <lynchmarc2@gmail.com>
2020-04-29 12:53:58 -0700

    refactor(cli): resultsFile arg exported

--- packages/cli/src/helpers/args.ts
+++ packages/cli/src/helpers/args.ts
@@ -3,3 +3,9 @@ export const harpath = {
   required: true,
   description: "The path to the HTTP Archive File (HAR)",
 };
+
+export const resultsFile = {
+  name: "resultsFile",
+  required: true,
+  description: `The "tracerbench compare" command json output file`,
+};
