1fcf05460e5cee95edd2f400c93272fa2b8bff40
Kris Selden <kris.selden@gmail.com>
2020-06-24 14:50:54 -0700

    refactor(core/test): migrate to mocha

--- packages/core/test/build/build.ts
+++ packages/core/test/build/build.ts
@@ -1,60 +1,31 @@
 import { mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
 import compileTemplates from './compile_templates';
 import makePrecompile from './make_precompile';
 
-export default function build(channels: string[]) {
+export default function build(channels: string[], root: string) {
   channels.forEach((channel) => {
     console.log(`building fixture app for "${channel}"...`);
-    const emberjs = readFileSync(
-      `test/vendor/ember-${channel}/dist/ember.prod.js`,
-      'utf8'
-    );
+    const vendorDir = join(root, `test/vendor/ember-${channel}/dist`);
+    const emberjs = readFileSync(join(vendorDir, 'ember.prod.js'), 'utf8');
     const precompile = makePrecompile(
-      `test/vendor/ember-${channel}/dist/ember-template-compiler.js`
+      join(vendorDir, 'ember-template-compiler.js')
     );
     const templates = compileTemplates(precompile);
+    const fixturesDir = join(root, 'test/fixtures');
     const appjs =
-      templates + '\n' + readFileSync('test/fixtures/app.js', 'utf8');
-    const index = readFileSync('test/fixtures/index.html', 'utf8');
+      templates + '\n' + readFileSync(join(fixturesDir, 'app.js'), 'utf8');
+    const index = readFileSync(join(fixturesDir, 'index.html'), 'utf8');
 
-    const dir = `dist/test/${channel}`;
+    const testDir = join(root, `dist/test/${channel}`);
     try {
-      mkdirSync(dir);
+      mkdirSync(testDir);
     } catch (e) {
       // ignore
     }
-    const pwd = process.cwd();
-    try {
-      process.chdir(dir);
 
-      writeFileSync('ember.prod.js', emberjs);
-      writeFileSync('app.js', appjs);
-      writeFileSync('index.html', index);
-
-      // todo: need to address uglify-js non gt es5 support
-      // either transpile before down to es5 or leverage
-      // a different library than uglify-js
-      // kicking the can on minification ATM as non-critical
-
-      // let result = minify("ember.prod.js", {
-      //   output: {
-      //     semicolons: false
-      //   } as any,
-      //   sourceMap: true
-      // });
-      // writeFileSync("ember.min.js", result.code);
-      // writeFileSync("ember.min.map", result.map);
-
-      // result = minify("app.js", {
-      //   output: {
-      //     semicolons: false
-      //   } as any,
-      //   sourceMap: true
-      // });
-      // writeFileSync("app.min.js", result.code);
-      // writeFileSync("app.min.map", result.map);
-    } finally {
-      process.chdir(pwd);
-    }
+    writeFileSync(join(testDir, 'ember.prod.js'), emberjs);
+    writeFileSync(join(testDir, 'app.js'), appjs);
+    writeFileSync(join(testDir, 'index.html'), index);
   });
 }
--- packages/core/test/build/copy_jquery.ts
+++ packages/core/test/build/copy_jquery.ts
@@ -1,16 +1,17 @@
 import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
 
-export default function copyJquery() {
+export default function copyJquery(root: string) {
   writeFileSync(
-    "dist/test/jquery.js",
+    join(root, "dist/test/jquery.js"),
     readFileSync(require.resolve("jquery/dist/jquery"), "utf8")
   );
   writeFileSync(
-    "dist/test/jquery.min.js",
+    join(root, "dist/test/jquery.min.js"),
     readFileSync(require.resolve("jquery/dist/jquery.min"), "utf8")
   );
   writeFileSync(
-    "dist/test/jquery.min.map",
+    join(root, "dist/test/jquery.min.map"),
     readFileSync(require.resolve("jquery/dist/jquery.min.map"), "utf8")
   );
 }
