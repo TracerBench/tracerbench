68ffafab06f6565514c3d3c6e37f806ca69a6244
Kris Selden <kris.selden@gmail.com>
2020-07-23 20:50:23 -0700

    fix: make setting flags safer

--- packages/core/src/util/gc.ts
+++ packages/core/src/util/gc.ts
@@ -1,14 +1,20 @@
 import { setFlagsFromString } from 'v8';
 import { runInNewContext } from 'vm';
+import debug = require('debug');
+const debugCallback = debug('tracerbench:gc');
 // avoid v8 doing work while we are idling waiting for the sample to complete
-setFlagsFromString(
-  [
-    '--predictable',
-    '--predictable-gc-schedule',
-    '--single-threaded',
-    '--single-threaded-gc',
-    '--expose-gc'
-  ].join(' ')
+['--predictable', '--single-threaded', '--single-threaded-gc'].forEach(
+  (flag) => {
+    try {
+      setFlagsFromString(flag);
+    } catch (e) {
+      debugCallback('error setting flag %o', e);
+    }
+  }
 );
-const gc: () => void = runInNewContext('gc');
-export default gc;
+setFlagsFromString('--expose-gc');
+const _gc: () => void = runInNewContext('gc');
+export default function gc(): void {
+  debugCallback('full gc');
+  _gc();
+}
