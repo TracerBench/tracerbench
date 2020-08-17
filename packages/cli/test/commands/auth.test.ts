ed1daf965752699c53fb8605c52cbb09eb950d42
Marc Lynch <lynchmarc2@gmail.com>
2020-08-17 09:21:19 -0700

    test(cli): record-har:auth cmd

---
+++ packages/cli/test/commands/auth.test.ts
@@ -0,0 +1,14 @@
+import { test } from "@oclif/test";
+import { expect } from "chai";
+
+import RecordHARAuth from "../../src/commands/record-har/auth";
+
+describe("record-har:auth without arg on throw error", () => {
+  test.stderr().it(`runs record-har:auth`, async () => {
+    try {
+      await RecordHARAuth.run([]);
+    } catch (error) {
+      expect(`${error}`).to.contain(`Missing required flag`);
+    }
+  });
+});
