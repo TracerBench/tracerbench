f8ce9ecd6ffe4af0b277a6a19433f51c5f2308cb
timamura <timamura@linkedin.com>
2021-11-22 13:42:59 -0800

    chore: bump deps to latest

--- packages/cli/test/commands/record-har.test.ts
+++ packages/cli/test/commands/record-har.test.ts
@@ -39,7 +39,7 @@ describe("record-har headless", () => {
         expect(ctx.stdout).to.contain(`HAR recorded`);
         expect(harJSON.log.entries.length).to.be.gt(1);
         expect(harJSON.log.entries[0].request.url).to.contain(`${URL}`);
-        expect(harJSON.log.entries[0].request.headers.length).to.be.gt(1);
+        expect(harJSON.log.entries[0].request.headers.length).eq(1);
         expect(harJSON.log.entries[0].response.headers.length).to.be.gt(1);
 
         assert.exists(screenshotPath);
