7e1ca9ae3c5d1c2cfbaa5dae6a43a0bf60cf2cec
timamura <timamura@linkedin.com>
2022-08-09 15:05:58 -0700

    feat: end recording trace at LCP if marker specifies

--- packages/cli/test/commands/record-har.test.ts
+++ packages/cli/test/commands/record-har.test.ts
@@ -1,5 +1,6 @@
 import { test } from "@oclif/test";
 import { Archive } from "@tracerbench/har";
+import { LCP_EVENT_NAME_ALIAS } from "@tracerbench/core";
 import { expect, assert } from "chai";
 import { describe } from "mocha";
 
@@ -25,6 +26,50 @@ describe("record-har headless", () => {
           COOKIES,
           "--filename",
           FILENAME,
+          "--headless",
+          "--screenshots"
+        ]);
+
+        const harFile = join(TB_RESULTS_FOLDER, `${FILENAME}.har`);
+        const screenshotPath = join(
+          TB_RESULTS_FOLDER,
+          "record-har-app-screenshot.png"
+        );
+        const harJSON: Archive = readJSONSync(harFile);
+
+        expect(ctx.stdout).to.contain(`app screenshot:`);
+        expect(ctx.stdout).to.contain(`Validating HAR`);
+        expect(ctx.stdout).to.contain(`HAR recorded`);
+        expect(harJSON.log.entries.length).to.be.gt(1);
+        expect(harJSON.log.entries[0].request.url).to.contain(`${URL}`);
+        expect(harJSON.log.entries[0].request.headers.length).eq(1);
+        expect(harJSON.log.entries[0].response.headers.length).to.be.gt(1);
+
+        assert.exists(screenshotPath);
+        assert.exists(harFile);
+        assert.equal(harJSON.log.creator.name, "TracerBench");
+        assert.equal(harJSON.log.entries[0].response.status, 200);
+      }
+    );
+});
+
+describe("record-har end recording at LCP", () => {
+  test
+    .stdout()
+    .it(
+      `runs record-har --url ${URL} --dest ${TB_RESULTS_FOLDER} --cookiespath ${COOKIES} --filename ${FILENAME} --marker ${LCP_EVENT_NAME_ALIAS} --headless --screenshots`,
+      async (ctx) => {
+        await RecordHAR.run([
+          "--url",
+          URL,
+          "--dest",
+          TB_RESULTS_FOLDER,
+          "--cookiespath",
+          COOKIES,
+          "--filename",
+          FILENAME,
+          "--marker",
+          LCP_EVENT_NAME_ALIAS,
           "--headless",
           "--screenshots"
         ]);
